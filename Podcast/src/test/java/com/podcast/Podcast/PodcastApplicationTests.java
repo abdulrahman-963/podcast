2aabb6a63290346957429753af36031515e0c429
abdulrahman <abdulrahmanalkilani1@outlook.com>
2025-01-21 21:58:50 +0300

    first commit

---
+++ Podcast/src/test/java/com/podcast/Podcast/PodcastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.podcast.Podcast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodcastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
